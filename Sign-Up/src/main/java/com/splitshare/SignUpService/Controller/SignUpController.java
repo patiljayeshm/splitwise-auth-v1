d67aac49ccb012f972733aaf56b66938d23f0583
Ancientblack <patiljayeshm@outlook.com>
2024-05-11 08:17:05 +0530

    initial

---
+++ Sign-Up/src/main/java/com/splitshare/SignUpService/Controller/SignUpController.java
@@ -0,0 +1,8 @@
+package com.splitshare.SignUpService.Controller;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/v1/")
+public class SignUpController {
+}
---
+++ Sign-Up/src/main/java/com/splitshare/SignUpService/SignUpServiceApplication.java
@@ -0,0 +1,13 @@
+package com.splitshare.SignUpService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SignUpServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SignUpServiceApplication.class, args);
+	}
+
+}
